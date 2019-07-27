c125fcc9ccf5d8864b8add9f3d4c09017f990fee
Gitguo <641019982@qq.com>
2019-07-27 16:29:41 +0800

    common

---
+++ changgou-parent/changgou-common/src/main/java/com/changgou/entity/CacheKey.java
@@ -0,0 +1,25 @@
+package com.changgou.entity;
+
+/****
+ * @Author:shenkunlin
+ * @Description:
+ * @Date 2019/6/18 12:02
+ *****/
+public class CacheKey {
+
+    /**
+     * 商品分类
+     */
+    public static String CATEGORY="CATEGORY";
+
+    /**
+     * 品牌缓存
+     */
+    public static String BRAND="BRAND";
+
+    /**
+     * 规格
+     */
+    public static String SPEC="SPEC";
+
+}
