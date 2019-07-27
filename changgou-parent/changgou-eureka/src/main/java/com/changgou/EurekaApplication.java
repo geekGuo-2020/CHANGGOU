21bdc7ebf98558a16190b0d28d5a65c964b2ec29
Gitguo <641019982@qq.com>
2019-07-27 16:18:03 +0800

    Eureka微服务

---
+++ changgou-parent/changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author: 郭师兄
+ * @date: 2019/7/26 16:44
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
